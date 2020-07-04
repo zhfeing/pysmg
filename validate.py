c822477c6f0f62ed3f8393592f8ecd29d4e61d20
zhfeing <zhf666968@gmail.com>
2020-07-04 09:46:16 +0800

    clean up

--- validate.py
+++ validate.py
@@ -1,13 +1,11 @@
 import yaml
-import torch
 import argparse
 import timeit
-import numpy as np
 
+import torch
 from torch.utils import data
 
-
-from ptsemseg.models import get_model
+from ptsemseg.model import get_model
 from ptsemseg.data import get_dataset
 from ptsemseg.metrics import RunningScore
 from ptsemseg.utils import convert_state_dict
@@ -15,10 +13,8 @@ from ptsemseg.utils import convert_state_dict
 torch.backends.cudnn.benchmark = True
 
 
+@torch.no_grads()
 def validate(cfg, args):
-
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
     # Setup Dataloader
     data_loader = get_dataset(cfg["data"]["dataset"])
     data_path = cfg["data"]["path"]
@@ -27,44 +23,43 @@ def validate(cfg, args):
         data_path,
         split=cfg["data"]["val_split"],
         is_transform=True,
-        img_size=(cfg["data"]["img_rows"], cfg["data"]["img_cols"]),
+        img_size=(cfg["data"]["img_rows"], cfg["data"]["img_cols"])
     )
 
     n_classes = loader.n_classes
 
-    valloader = data.DataLoader(loader, batch_size=cfg["training"]["batch_size"], num_workers=8)
+    valloader = data.DataLoader(
+        loader,
+        batch_size=cfg["validation"]["batch_size"],
+        num_workers=cfg["validation"]["n_workers"],
+        shuffle=False,
+        pin_memory=True
+    )
     running_metrics = RunningScore(n_classes)
 
     # Setup Model
 
-    model = get_model(cfg["model"], n_classes).to(device)
-    state = convert_state_dict(torch.load(args.model_path)["model_state"])
+    model = get_model(cfg["model"], n_classes)
+    # data parrall model state
+    state = torch.load(args.model_path, map_location="cpu")["model_state"]
+    state = convert_state_dict(state)
     model.load_state_dict(state)
+    model = torch.nn.DataParallel(model, device_ids=range(torch.cuda.device_count()))
     model.eval()
-    model.to(device)
 
     for i, (images, labels) in enumerate(valloader):
         start_time = timeit.default_timer()
 
-        images = images.to(device)
-
+        images = images.cuda()
         if args.eval_flip:
+            flipped_images = images.flip(-1)
+
             outputs = model(images)
-
-            # Flip images in numpy (not support in tensor)
-            outputs = outputs.cpu().numpy()
-            flipped_images = np.copy(images.cpu().numpy()[:, :, :, ::-1])
-            flipped_images = torch.from_numpy(flipped_images).float().to(device)
             outputs_flipped = model(flipped_images)
-            outputs_flipped = outputs_flipped.cpu().numpy()
-            outputs = (outputs + outputs_flipped[:, :, :, ::-1]) / 2.0
-
-            pred = np.argmax(outputs, axis=1)
+            outputs = (outputs + outputs_flipped.flip(-1)) / 2.0
         else:
             outputs = model(images)
-            pred = outputs.max(1)[1].cpu().numpy()
-
-        gt = labels.numpy()
+        pred = outputs.argmax(-1)
 
         if args.measure_time:
             elapsed_time = timeit.default_timer() - start_time
@@ -74,7 +69,7 @@ def validate(cfg, args):
                     i + 1, pred.shape[0] / elapsed_time
                 )
             )
-        running_metrics.update(gt, pred)
+        running_metrics.update(labels, pred)
 
     score, class_iou = running_metrics.get_scores()
 
@@ -136,6 +131,6 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     with open(args.config) as fp:
-        cfg = yaml.load(fp)
+        cfg = yaml.load(fp, yaml.SafeLoader)
 
     validate(cfg, args)
