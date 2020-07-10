0529fc46d0ecf431b949363a54faf1b4adecff0d
zhfeing <zhf666968@gmail.com>
2020-07-10 09:44:10 +0800

    run_config

--- run_p1.sh
+++ run_p1.sh
@@ -1,12 +1,12 @@
 export PYTHONPATH=./:/home/zhfeing/project/generate-models/segmentation_models.pytorch:$PYTHONPATH
 export TORCH_HOME=/home/zhfeing/model-zoo
 
-export CUDA_VISIBLE_DEVICES="2, 3"
+export CUDA_VISIBLE_DEVICES="1, 2"
 python model_generator.py \
     --global-config configs/global-config-p1.yml \
-    --cfg-path logs/test_p1/configs \
+    --cfg-path logs/run_p1/configs \
     --cfg-formatter arch-{}-encoder-{}-data-{}.yml \
-    --log-dir logs/test_p1/ \
+    --log-dir logs/run_p1/ \
     --gpu-preserve True \
     --debug False
 
--- run_p2.sh
+++ run_p2.sh
@@ -1,12 +1,12 @@
 export PYTHONPATH=./:/home/zhfeing/project/generate-models/segmentation_models.pytorch:$PYTHONPATH
 export TORCH_HOME=/home/zhfeing/model-zoo
 
-export CUDA_VISIBLE_DEVICES="0, 1"
+export CUDA_VISIBLE_DEVICES="3"
 python model_generator.py \
     --global-config configs/global-config-p2.yml \
-    --cfg-path logs/test_p2/configs \
+    --cfg-path logs/run_p2/configs \
     --cfg-formatter arch-{}-encoder-{}-data-{}.yml \
-    --log-dir logs/test_p2/ \
+    --log-dir logs/run_p2/ \
     --gpu-preserve True \
     --debug False
 
